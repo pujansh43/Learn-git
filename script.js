896e6c21edc8f9b86b4e4d93155a7db19913d298
Pujan Shrestha <pujanshh43@gmail.com>
2025-06-28 20:07:17 -0700

    Style the navbar list

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hello world");
 });
+
+const li = document.querySelectorAll("li");
+li.forEach((item) => {
+  item.style.color = "lightblue";
+});
